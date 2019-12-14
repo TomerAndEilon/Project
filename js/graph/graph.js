62fa9539d2b580e7b02ea7e18172efd2fed2a693
tomerar <45894448+tomerar@users.noreply.github.com>
2019-12-14 18:49:58 +0200

    add תציעו לי

---
+++ js/graph/graph.js
@@ -0,0 +1,230 @@
+class Vertex {
+    constructor(details) {
+        this.details = details;
+        this.name = details.name;
+        this.id = details.id;
+        this.condition = details.condition;
+        this.AdjInList = [];
+        this.AdjOutList = [];
+    }
+    addToAdjOutList(v) {
+        this.AdjOutList.push(v);
+    }
+    addToAdjInList(v) {
+        this.AdjInList.push(v);
+    }
+    getAdjOutList() {
+        return this.AdjOutList;
+    }
+    getAdjInList() {
+        return this.AdjInList;
+    }
+    deleteInCond(v) {
+        for (var i = 0; i < this.AdjInList.length; i++) {
+            if (v.getId() == this.AdjInList[i].getId()) {
+                this.AdjInList.splice(i, 1);
+                return;
+            }
+        }
+    }
+    getName() {
+        return this.name;
+    }
+    getCond() {
+        return this.condition;
+    }
+    getId() {
+        return this.id;
+    }
+}
+
+/********************************************************************************************************** */
+class Graph {
+
+    constructor() {
+        this.VertexList = [];
+    }
+    addVertex(v) {
+        this.VertexList.push(v);
+    }
+    addEdge(from, to) {
+        var vartexFrom = this.getFromVertexList(from.getId());
+        var vartexTo = this.getFromVertexList(to.getId());
+
+        vartexFrom.addToAdjOutList(vartexTo);
+        vartexTo.addToAdjInList(vartexFrom);
+    }
+
+    connectBetweenCourses() {
+        for (var i = 0; i < this.VertexList.length; i++) {
+            if (this.VertexList[i].getCond() != "") {
+                for (var j = 0; j < this.VertexList.length; j++) {
+                    if (this.VertexList[j].getId() == this.VertexList[i].getCond()) {
+                        g.addEdge(this.VertexList[j], this.VertexList[i]);
+                    }
+
+                }
+            }
+        }
+    }
+    connectBetweenCourseslist() {
+       
+
+        for (var i = 0; i < this.VertexList.length; i++) {
+            if ((Array.isArray(this.VertexList[i].getCond()) && this.VertexList[i].getCond().length)) {
+                for (const [key, condobj] of Object.entries(this.VertexList[i].getCond())) {
+
+                    for (var j = 0; j < this.VertexList.length; j++) {
+                        if (this.VertexList[j].getId() == condobj) {
+                            this.addEdge(this.VertexList[j], this.VertexList[i]);
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+    find(id) {
+        for (const [key, vertex] of Object.entries(this.VertexList)) {
+            if (vertex.getId() == id) {
+                return vertex;
+            }
+        }
+        return "not found " + id
+    }
+    printGraph() {
+        for (var i = 0; i < this.VertexList.length; i++) {
+            var out = this.VertexList[i].getAdjOutList();
+            var conc = "";
+            for (var j = 0; j < out.length; j++)
+                conc += out[j].getName() + " ";
+
+            console.log("course:" + this.VertexList[i].getName() + " -> " + "in:" + conc);
+        }
+    }
+
+    printRelevantCourses() {
+        console.log('relvant courses')
+        for (var i = 0; i < this.VertexList.length; i++) {
+            var inCond = this.VertexList[i].getAdjInList();
+            if (inCond.length == 0)
+                console.log(this.VertexList[i].name + "  ");
+        }
+    }
+    getRelevantCourses() {
+      
+        var list_ver = new Array()
+        
+        for (var i = 0; i < this.VertexList.length; i++) {
+            var inCond = this.VertexList[i].getAdjInList();
+            if (inCond.length == 0){
+                list_ver.push(this.VertexList[i]);
+            }
+        }
+        return list_ver
+    }
+
+    getFromVertexList(id) {
+        for (var i = 0; i < this.VertexList.length; i++) {
+            if (this.VertexList[i].getId() == id)
+                return this.VertexList[i];
+        }
+    }
+    deleteNode(nodeId) {
+        for (var i = 0; i < this.VertexList.length; i++) {
+            if (this.VertexList[i].getId() == nodeId) {
+                var out = this.VertexList[i].getAdjOutList();
+                for (var j = 0; j < out.length; j++) {
+                    var vertex = this.getFromVertexList(out[j].getId());
+                    vertex.deleteInCond(this.VertexList[i]);
+                }
+                this.VertexList.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+    // bfs(v) 
+    // dfs(v) 
+}
+
+// var g = new Graph();
+// //http://myjson.com/ you can change it with this web
+// requestURL = "https://tomerandeilon.github.io/Project/datajson.json"
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'text'; // now we're getting a string!
+// request.send();
+
+// request.onload = function () {
+//     const arrText = request.response; // get the string from the response
+//     const arr = JSON.parse(arrText); // convert it to an object
+//     console.log(arr);
+//     runG(arr);
+// };
+
+// var arr = [{
+//     "id": 1,
+//     "name": "פייתון",
+//     "condition": [],
+// },
+// {
+//     "id": 2,
+//     "name": "לינארית",
+//     "condition": [],
+// },
+// {
+//     "id": 3,
+//     "name": "ספרתיות",
+//     "condition": [],
+// },
+// {
+//     "id": 4,
+//     "name": "דיסקרטית",
+//     "condition": [],
+// },
+// {
+//     "id": 5,
+//     "name": "מודרנית",
+//     "condition": ["3"],
+// },
+// {
+//     "id": 6,
+//     "name": "c++",
+//     "condition": [],
+// }];
+// var vertices = JSON.stringify(arr);
+// console.log(arr);
+// runG(arr)
+// function runG(arr) {
+//     for (i in arr) {
+//         g.addVertex(new Vertex(arr[i]));
+//     }
+
+//     const event = new Date();
+
+//     console.log(event.toString());
+
+//     g.connectBetweenCourseslist();
+//     g.printGraph();
+//     // g.printRelevantCourses();
+//     // console.log("now we finish with python and discrtit");
+//     // g.deleteNode(1);
+//     // g.deleteNode(4);
+//     // g.printRelevantCourses();
+//     console.log(g.VertexList);
+//     console.log(g.find("1"));
+//     console.log(g.find("5"));
+//     console.log("=====");
+
+//     g.printRelevantCourses();
+//     console.log("=====");
+//     g.deleteNode(3);
+//     console.log(g.find("1"));
+
+//     console.log("=====");
+
+//     g.printRelevantCourses();
+//     console.log("=====");
+// }
--- js/schedule.js
+++ js/schedule.js
@@ -2,86 +2,116 @@
 
 console.log("what hepand")
 $(document).ready(function () {
-    is_login()
-    logout()
-    update_data_to_profile()
+    console.log("start");
+    is_login();
+    logout();
+    update_data_to_profile();
     console.log("ready!");
-    choose_course() 
-    choose_course_number()
+    choose_course();
+    choose_course_number();
+    make_graph();
 
 
-   
-    
+
+
     console.log("end!");
 });
 
-// function is_login() {
-//     var firebaseConfig = {
-//         apiKey: "AIzaSyC5TD5bZiZz40XmGIFdjM5nIwga_QBTlBM",
-//         authDomain: "projectet-8baf2.firebaseapp.com",
-//         databaseURL: "https://projectet-8baf2.firebaseio.com",
-//         projectId: "projectet-8baf2",
-//         storageBucket: "projectet-8baf2.appspot.com",
-//         messagingSenderId: "614014308460",
-//         appId: "1:614014308460:web:5650642485d3e60f3dc849",
-//         measurementId: "G-EF5TVMPS6S"
-//     };
-//     // Initialize Firebase
-//     firebase.initializeApp(firebaseConfig);
-//     if (!firebase.apps.length) {
-//         firebase.initializeApp({});
-//     }
+function make_graph() {
+    function add_to_html_file_data_coures(list_vertex) {
+        $("#table-coures").empty();
+        $("#table-coures").append(
+            '<thead " class="thead-dark">'+
+            '<tr>'+
+            '<th scope="col">#</th>'+
+            '<th scope="col">קורסים</th>'+
+            '<th scope="col">נקודות זכות</th>'+
+            '</tr>'+
+            '</thead>'+
+            '<tbody id="in-data-table"></tbody>');
 
-//     var userNow2
-
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-//             userNow2 = user
-//             console.log(user);
-           
-//         } else {
-
-//             console.log("not loging");
-//         }
-//     });
-// }
-
-function if_have_user_name_show_me(user){
-    setTimeout(function(){
-        if (user.displayName != null){
-            $("#hello-user").text("שלום "+ user.displayName);
-            $("#hello-user").css("display","block");
+        for (let index = 0; index < list_vertex.length; index++) {
+            
+            $("#in-data-table").append(
+                '<tr>'+
+                '<th scope="row">'+(index+1)+'</th>' +
+                '<td>'+list_vertex[index].getName()+'</td>'+
+                '<td>5</td>'+  
+            ' </tr>'
+            ); 
         }
-    },1000);
-    
+        $("#table-row").css("display", "block");
+        
+    }
+    $("#choose-for-me-btn").on("click", function () {
+        var g = new Graph();
+        requestURL = "https://tomerandeilon.github.io/Project/datajson.json"
+        let request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'text'; // now we're getting a string!
+        request.send();
+
+        request.onload = function () {
+            const arrText = request.response; // get the string from the response
+            const arr = JSON.parse(arrText); // convert it to an object
+            runG(arr);
+        };
+
+       
+        function runG(arr) {
+            for (i in arr) {
+                g.addVertex(new Vertex(arr[i]));
+            }
+            const event = new Date();
+            g.connectBetweenCourseslist();
+            // g.printGraph();
+            var list_vertex = g.getRelevantCourses()
+            
+            add_to_html_file_data_coures(list_vertex)
+            
+            
+        }
+
+
+    });
 }
-function logout(){
+
+function if_have_user_name_show_me(user) {
+    setTimeout(function () {
+        if (user.displayName != null) {
+            $("#hello-user").text("שלום " + user.displayName);
+            $("#hello-user").css("display", "block");
+        }
+    }, 1000);
+
+}
+function logout() {
     $("#logout-btn").on("click", function () {
         var user = firebase.auth().currentUser;
         firebase.auth().signOut().then(function () {
-          console.log("logout successful")
+            console.log("logout successful")
         }).catch(function (error) {
-          console.log("logout error " + error);
+            console.log("logout error " + error);
         });
-      });
+    });
 }
-function update_data_to_profile(){
+function update_data_to_profile() {
     setTimeout(function () {
         var user = firebase.auth().currentUser;
         console.log(user.displayName)
         console.log(localStorage.getItem("fullname"))
-        if (user.displayName == null){
+        if (user.displayName == null) {
             user.updateProfile({
                 displayName: localStorage.getItem("fullname")
-              }).then(function () {
+            }).then(function () {
                 console.log("work update user name");
-              }).catch(function (error) {
+            }).catch(function (error) {
                 // An error happened.
                 console.log("error update user name");
-              });
+            });
         }
         if_have_user_name_show_me(user)
-    },3000);
+    }, 3000);
 }
 function is_login() {
     var firebaseConfig = {
@@ -108,9 +138,9 @@ function is_login() {
             console.log(user);
             $("#logout-btn").css("display", "block");
             $("#login-btn").css("display", "none");
-            
+
         } else {
-            $("#hello-user").css("display","none");
+            $("#hello-user").css("display", "none");
             $("#logout-btn").css("display", "none");
             $("#login-btn").css("display", "block");
             console.log("not loging");
@@ -122,7 +152,7 @@ function is_login() {
 function choose_course() {
     var mySelect = $('#choose-course');
     $('#choose-info').on('click', function () {
-        localStorage.setItem("course_allready_done",mySelect.selectpicker('val'))
+        localStorage.setItem("course_allready_done", mySelect.selectpicker('val'))
         console.log(mySelect.selectpicker('val'));
         mySelect.selectpicker('refresh');
     });
@@ -136,7 +166,7 @@ function choose_course() {
 function choose_course_number() {
     var mySelect = $('#choose-course-number');
     $('#choose-info-number').on('click', function () {
-        localStorage.setItem("course_number",mySelect.selectpicker('val'))
+        localStorage.setItem("course_number", mySelect.selectpicker('val'))
         console.log(mySelect.selectpicker('val'));
         mySelect.selectpicker('refresh');
     });
